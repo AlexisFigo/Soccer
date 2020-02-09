6e3d0665fd2bb87f027524243ad703acde63743d
Alexis Correa <figo1813@gmail.com>
2020-02-09 20:35:49 +0100

    GroupEntity and TournamentEntity  were added

---
+++ Soccer.Web/Data/Entities/TournamentEntity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.Web.Data.Entities
+{
+    public class TournamentEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Name { get; set; }
+
+        //we always keep the time in london format
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = false)]
+        public DateTime StartDate { get; set; }
+
+        //This property helps us recover the time in locar format
+        [Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = false)]
+        public DateTime StartDateLocal => StartDate.ToLocalTime();
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "End Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = false)]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "End Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = false)]
+        public DateTime EndDateLocal => EndDate.ToLocalTime();
+
+        [Display(Name = "Is Active?")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "Logo")]
+        public string LogoPath { get; set; } 
+
+        //This collection allows us to manipulate data both ways
+
+        //one tournamenEntity have many group and one group belongs to tournament
+        public ICollection<GroupEntity> Groups { get; set; }
+    }
+}
