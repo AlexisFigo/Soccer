753fb18ad11c1a64b560d7fe2d54833c265d5686
Alexis Correa <figo1813@gmail.com>
2020-02-09 20:14:45 +0100

    teamController is added

---
+++ Soccer.Web/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Soccer.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Soccer.Web.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        //I must always add the tables I will work with
+        public DbSet<TeamEntity> Teams { get; set; }
+    }
+}
